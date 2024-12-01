8f5c6e86ea20caebe4f591185296f8c12414a16e
LucasF <franco100lucas@gmail.com>
2024-12-01 09:19:36 -0300

    Feat: Add Authorization before enter in some controller methods

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,10 @@
 class OrdersController < ApplicationController
-  include CurrentCart
-
+  skip_before_action :authorize, only: [:create, :new]
   before_action :set_cart, only: %i[ new create ]
   before_action :ensure_cart_isnt_empty, only: %i[ new ]
   before_action :set_order, only: %i[ show edit update destroy ]
+  
+  include CurrentCart
 
   # GET /orders or /orders.json
   def index
